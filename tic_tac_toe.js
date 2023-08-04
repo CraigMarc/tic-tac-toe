a3208089ec885c5b4cb0eca7aa2c0a1c2d39b38f
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-04 19:26:49 -0400

    added check winner function

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -11,7 +11,7 @@ const Gameboard = (() => {
         gameboardArr[index] = sign;
     };
 
-    
+
     return { gameboardArr, setField };
 })();
 
@@ -23,7 +23,7 @@ const Gameboard = (() => {
 /*gamecontroller module*/
 
 const Gamecontroller = (() => {
-    
+
     let counter = 0
 
     /*event listener*/
@@ -35,20 +35,20 @@ const Gamecontroller = (() => {
         cell.addEventListener('click', (e) => {
             let value = cell.id
 
-            if(counter % 2 == 0  && Gameboard.gameboardArr[value] == "") {
+            if (counter % 2 == 0 && Gameboard.gameboardArr[value] == "") {
                 Gameboard.setField(value, "X")
             }
-            
+
             // if the number is odd
             else if (Gameboard.gameboardArr[value] == "") {
                 Gameboard.setField(value, "O")
             }
-            
+
             counter = counter + 1
-            console.log(counter)
+
             setBoard()
             console.log(Gameboard.gameboardArr)
-
+            console.log(checkWinner())
             if (counter == 9) {
                 let message = document.getElementById('message')
                 message.textContent = "Tie"
@@ -61,13 +61,37 @@ const Gamecontroller = (() => {
     const setBoard = () => {
         for (let i = 0; i < Gameboard.gameboardArr.length; i++) {
             let cell = document.getElementById(i);
-            
+
             cell.textContent = Gameboard.gameboardArr[i];
         }
 
     }
 
-    
+    const checkWinner = () => {
+        const winner = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8]
+            [2, 4, 6]
+        ]
+       
+        for (let i = 0; i < winner.length; i++) {
+            if (Gameboard.gameboardArr[winner[i][0]] == Gameboard.gameboardArr[winner[i][1]]
+                 && Gameboard.gameboardArr[winner[i][1]] == Gameboard.gameboardArr[winner[i][2]]) {
+            return 'winner'
+        }
+        else {return 'loser'
+
+    }
+       
+
+    }
+
+    }
 
     return { counter };
 
