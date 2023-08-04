566ae90c09ae0cf3917758ac01a104a98b1ee811
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-04 17:13:04 -0400

    added stuff

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,43 +1,54 @@
-const Gameboard = ((player, position) => {
+/*gameBoard module*/
+
+const Gameboard = (() => {
     let gameboardArr = ["", "", "", "", "", "", "", "", ""]
 
-    
+    /*changes array to set player choice*/
     const setField = (index, sign) => {
-        
-        gameboardArr[index] = sign;
-      };
-    
 
-    return { player, position, gameboardArr, setField };
+        gameboardArr[index] = sign;
+    };
+
+
+    return { gameboardArr, setField };
 })();
 
 
-Gameboard.setField(0, "cra")
-console.log(Gameboard.gameboardArr)
+
+
+
 
 /*gamecontroller module*/
-const Gamecontroller = ((player, position) => {
+
+const Gamecontroller = (() => {
 
 
-/*event listener*/
+    /*event listener*/
 
-const number = document.querySelectorAll('.cell');
+    const number = document.querySelectorAll('.cell');
 
     number.forEach((cell) => {
 
-      cell.addEventListener('click', (e) => {
-        let value = cell.id
-        Gameboard.setField(value, "cr")
-       setBoard()
-      })
+        cell.addEventListener('click', (e) => {
+            let value = cell.id
+            Gameboard.setField(value, "cr")
+            setBoard()
+            console.log(Gameboard.gameboardArr)
+        })
     })
-/*set board*/
-const setBoard = () => {
-    for (let i = 0; i < Gameboard.gameboardArr.length; i++) {
-        let cell = document.getElementById(i);
 
-        cell.textContent = Gameboard.gameboardArr[i];
+    /*set board*/
+    const setBoard = () => {
+        for (let i = 0; i < Gameboard.gameboardArr.length; i++) {
+            let cell = document.getElementById(i);
+
+            cell.textContent = Gameboard.gameboardArr[i];
+        }
+
     }
 
-}
+    
+
+
+
 })();
